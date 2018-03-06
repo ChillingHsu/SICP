f207344538f5f4fde7fec979ad3521234477cec7
Chilling Hsu <chillinghsu@outlook.com>
2018-03-06 23:17:23 +0800

    Update to ex3.39

---
+++ Chapter 3/share-account.scm
@@ -0,0 +1,29 @@
+;ex3.38
+;TODO
+(define balance 100)
+
+(set! balance (+ balance 10))
+(set! balance (- balance 20))
+(set! balance (- balance (/ balance 2)))
+balance
+
+(define balance 100)
+
+(set! balance (+ balance 10))
+(set! balance (- balance (/ balance 2)))
+(set! balance (- balance 20))
+balance
+
+(define balance 100)
+
+(set! balance (- balance (/ balance 2)))
+(set! balance (+ balance 10))
+(set! balance (- balance 20))
+balance
+
+;ex3.39
+; 过程P1(lambda() (set! x ((s (lambda () (* x x))))))
+; 过程P2(s (lambda () (set! x (+ x 1))))
+; 100: P1访问x，而后P2将x设置为11，而后P1将x设置为100
+; 101: P1将x设置为100，而后P2将x设置为101
+; 121: P2将x设置为11，而后P1将x设置为121
