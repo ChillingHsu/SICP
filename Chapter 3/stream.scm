7dab89cb84842bf260f72b0899ad644df2536cc1
Chilling Hsu <chillinghsu@outlook.com>
2018-03-11 16:47:29 +0800

    Update to ex3.60

--- Chapter 3/stream.scm
+++ Chapter 3/stream.scm
@@ -277,3 +277,14 @@ sum ;Value: 210
 (define cosine-series
   (cons-stream 1 (integrate-series sine-series)))
 (display-stream cosine-series 10)
+
+;ex3.60
+(define (mul-series s1 s2)
+  (cons-stream (* (stream-car s1)
+                  (stream-car s2))
+               (add-streams
+                 (scale-stream (stream-cdr s2) (stream-car s1))
+                 (mul-series (stream-cdr s1) s2))))
+(define one (add-streams (mul-series cosine-series cosine-series)
+                         (mul-series sine-series sine-series)))
+(display-stream (partial-sums one) 10)
