458e6a4ac46d6a7b3699176d75b52e815d2d3d8f
Chilling Hsu <chillinghsu@outlook.com>
2018-03-12 21:56:28 +0800

    Update to ex3.62

--- Chapter 3/stream.scm
+++ Chapter 3/stream.scm
@@ -306,5 +306,40 @@ sum ;Value: 210
                    (evaluate (stream-cdr series) (* x coeff) new-accum))))
   (evaluate series 1 0))
 
-(display-stream (evaluate tan-series 1.) 100)
+(display-stream (evaluate tan-series 1.) 10)
 (tan 1)
+
+(define (average a b) (/ (+ a b) 2))
+(define (sqrt-stream x)
+  (define (sqrt-improve guess x)
+    (average guess (/ x guess)))
+  (define guesses
+    (cons-stream 1.0
+                 (stream-map (lambda (guess)
+                               (sqrt-improve guess x))
+                              guesses)))
+  guesses)
+(display-stream (sqrt-stream 2) 10)
+
+(define (pi-summands n)
+  (cons-stream (/ 1 n)
+               (stream-map - (pi-summands (+ n 2)))))
+(define pi-stream
+  (scale-stream (partial-sums (pi-summands 1.)) 4))
+(display-stream pi-stream 10)
+
+(define (euler-transform s)
+  (let ((s0 (stream-ref s 0))
+        (s1 (stream-ref s 1))
+        (s2 (stream-ref s 2)))
+    (cons-stream (- s2 (/ (square (- s2 s1))
+                          (+ s0 (* -2 s1) s2)))
+                 (euler-transform (stream-cdr s)))))
+(display-stream (euler-transform pi-stream) 10)
+
+(define (make-tableau transform s)
+  (cons-stream s
+               (make-tableau transform (transform s))))
+(define (accelerated-sequence transform s)
+  (stream-map stream-car (make-tableau transform s)))
+(display-stream (accelerated-sequence euler-transform pi-stream) 10)
